dd29205c391d857dd72125009c9dc386af47fa4b
ReQ <mrzielony22@gmail.com>
2023-08-16 14:41:45 +0200

    cleared code

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { onMount } from 'svelte'
     import { Character, getCharacters } from './apiRequests'
-    import Loader from './views/Loader.svelte'
+    import Loader from './elements/Loader.svelte'
 
     let characters: Character[] = []
     let isLoading = true
